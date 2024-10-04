221fb20c5edf73614dfcf902d7e5ec9a6fa1ba38
Tyler Hood <tyler.josephhood@gmail.com>
2024-10-04 15:26:54 -0400

    Adding backend methods to grab mongoDB data in sorted format.

--- src/main/java/superioraccountingsoftware/com/Accounting/AccountController.java
+++ src/main/java/superioraccountingsoftware/com/Accounting/AccountController.java
@@ -16,4 +16,58 @@ public class AccountController {
     public ResponseEntity<List<Accounts>> getAllAccounts() { //Getting request from user and returning a response
         return new ResponseEntity<>(accountService.getAllAccounts(), HttpStatus.OK); //gets all users from DB and giving it to API Layer
     }
+
+    @GetMapping("/accountNumberASC")
+    public ResponseEntity<List<Accounts>> getByAccountNumberASC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountNumberASC(), HttpStatus.OK);
+    }
+
+    @GetMapping("/accountNumberDESC")
+    public ResponseEntity<List<Accounts>> getByAccountNumberDESC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountNumberDESC(), HttpStatus.OK);
+    }
+
+    @GetMapping("/accountNameASC")
+    public ResponseEntity<List<Accounts>> getByAccountNameASC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountNameASC(), HttpStatus.OK);
+    }
+
+    @GetMapping("/accountNameDESC")
+    public ResponseEntity<List<Accounts>> getByAccountNameDESC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountNameDESC(), HttpStatus.OK);
+    }
+    @GetMapping("/orderASC")
+    public ResponseEntity<List<Accounts>> getByAccountOrderASC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountOrderASC(), HttpStatus.OK);
+    }
+    @GetMapping("/orderDESC")
+    public ResponseEntity<List<Accounts>> getByAccountOrderDESC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountOrderDESC(), HttpStatus.OK);
+    }
+    @GetMapping("/accountCategoryASC")
+    public ResponseEntity<List<Accounts>> getByAccountCategoryASC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountCategoryASC(), HttpStatus.OK);
+    }
+
+    @GetMapping("/accountCategoryDESC")
+    public ResponseEntity<List<Accounts>> getByAccountCategoryDESC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountCategoryDESC(), HttpStatus.OK);
+    }
+    @GetMapping("/accountSubCategoryASC")
+    public ResponseEntity<List<Accounts>> getByAccountSubCategoryASC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountSubCategoryASC(), HttpStatus.OK);
+    }
+
+    @GetMapping("/accountSubCategoryDESC")
+    public ResponseEntity<List<Accounts>> getByAccountSubCategoryDESC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountSubCategoryDESC(), HttpStatus.OK);
+    }
+    @GetMapping("/balanceASC")
+    public ResponseEntity<List<Accounts>> getByAccountBalanceASC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountBalanceASC(), HttpStatus.OK);
+    }
+    @GetMapping("/balanceDESC")
+    public ResponseEntity<List<Accounts>> getByAccountBalanceDESC() { //Getting request from user and returning a response
+        return new ResponseEntity<>(accountService.getByAccountBalanceDESC(), HttpStatus.OK);
+    }
 }
--- src/main/java/superioraccountingsoftware/com/Accounting/AccountService.java
+++ src/main/java/superioraccountingsoftware/com/Accounting/AccountService.java
@@ -2,6 +2,7 @@ package superioraccountingsoftware.com.Accounting;
 
 import java.util.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 //Class where most business logic will be written
@@ -13,5 +14,40 @@ public class AccountService {
     public List<Accounts> getAllAccounts() {
         return accountRepository.findAll(); //this method is in the MongoRepository class
     }
-
+    public List<Accounts> getByAccountNumberASC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.asc("accountNumber")));
+    }
+    public List<Accounts> getByAccountNumberDESC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.desc("accountNumber")));
+    }
+    public List<Accounts> getByAccountNameASC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.asc("accountName")));
+    }
+    public List<Accounts> getByAccountNameDESC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.desc("accountName")));
+    }
+    public List<Accounts> getByAccountOrderASC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.asc("order")));
+    }
+    public List<Accounts> getByAccountOrderDESC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.desc("order")));
+    }
+    public List<Accounts> getByAccountCategoryASC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.asc("accountCategory")));
+    }
+    public List<Accounts> getByAccountCategoryDESC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.desc("accountCategory")));
+    }
+    public List<Accounts> getByAccountSubCategoryASC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.asc("accountSubCategory")));
+    }
+    public List<Accounts> getByAccountSubCategoryDESC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.desc("accountSubCategory")));
+    }
+    public List<Accounts> getByAccountBalanceASC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.asc("balance")));
+    }
+    public List<Accounts> getByAccountBalanceDESC(){
+        return accountRepository.findAll(Sort.by(Sort.Order.desc("balance")));
+    }
 }
