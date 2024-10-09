6782de4dd2f2f4f838739b97fcbb76434a8d6379
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2024-10-09 18:14:09 -0400

    Even logs implemented. Now tracks when changes or creations are made.

--- src/main/java/superioraccountingsoftware/com/Accounting/EventsRepository.java
+++ src/main/java/superioraccountingsoftware/com/Accounting/EventsRepository.java
@@ -1,7 +1,6 @@
 package superioraccountingsoftware.com.Accounting;
 
 import org.bson.types.ObjectId;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
