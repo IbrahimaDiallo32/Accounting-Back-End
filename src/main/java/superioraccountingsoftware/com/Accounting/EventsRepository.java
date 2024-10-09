fde3d07dd37f509a45c6b3b9ac4001e77cecba92
Ibrahima Diallo <111662876+IbrahimaDiallo32@users.noreply.github.com>
2024-10-09 18:13:46 -0400

    Even logs implemented. Now tracks when changes or creations are made.

--- src/main/java/superioraccountingsoftware/com/Accounting/EventsRepository.java
+++ src/main/java/superioraccountingsoftware/com/Accounting/EventsRepository.java
@@ -4,17 +4,12 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 import java.util.Optional;
 
 
 
 @Repository
 public interface EventsRepository extends MongoRepository<Event, ObjectId> {
-
-    @Autowired
-    EventsRepository eventRepository = null;
 
     // Find Event by ObjectId
     Optional<Event> findById(ObjectId id);
